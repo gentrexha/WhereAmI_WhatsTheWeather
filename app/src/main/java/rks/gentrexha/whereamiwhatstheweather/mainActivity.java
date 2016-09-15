202a04d87b25f650babe08c53b92bb0da07dd064
Gent Rexha <gentr@stud.ntnu.no>
2016-09-15 16:43:00 +0200

    Application can now pinpoint your location through Google Maps API

--- app/src/main/java/rks/gentrexha/whereamiwhatstheweather/mainActivity.java
+++ app/src/main/java/rks/gentrexha/whereamiwhatstheweather/mainActivity.java
@@ -1,7 +1,10 @@
 package rks.gentrexha.whereamiwhatstheweather;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class mainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class mainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void btnMoreOnClick(View v) {
+        Button btnMore = (Button)findViewById(R.id.btnMore);
+        Intent intMap = new Intent(this, mapActivity.class);
+        startActivity(intMap);
     }
 }
